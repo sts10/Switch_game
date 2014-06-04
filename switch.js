4934e94a70b6a151d82e13b6e06546c35ff9039c
Sean Reed <seanreed1111@gmail.com>
2014-06-04 16:35:25 -0400

    initial commit

---
+++ switch.js
@@ -0,0 +1,14 @@
+// This week's game is called SWITCH! (It is a one player game.)
+
+// The game is played on a one-dimensional board 9 units long, placed either horizontally or vertically. 
+//(Let's assume it is horizontal for the rest of this description.)
+
+// The game starts with four red tokens and four black tokens grouped together on the left and right side of the board, respectively. 
+//The 9th space on the board (the middle space) starts off empty.
+
+// Each turn, the player can move any one token to an unoccupied adjacent space or jump over exactly one token
+//(of either color) to land on an unoccupied space.
+
+// Game Objective: Switch the pieces so that the black tokens are all on the left side of the board 
+// and the red tokens are on the right, in the shortest amount of time and shortest number of moves possible.
+"use strict";
